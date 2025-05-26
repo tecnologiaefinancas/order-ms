d7035a93c36ab052544a25458fa6f6895e010193
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-05-26 12:48:41 -0300

    RabbitMqConfig, OrderItemEvent, OrderCreatedListener and OrderCreatedEvent all of them is working.

--- src/main/java/com/tecnologiaefinancas/orderms/config/RabbitMqConfig.java
+++ src/main/java/com/tecnologiaefinancas/orderms/config/RabbitMqConfig.java
@@ -1,9 +1,25 @@
 package com.tecnologiaefinancas.orderms.config;
 
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
     public static final String ORDER_CREATED_QUEUE = "tf-order-created";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    //Criacao da fila
+    @Bean
+    public Declarable orderCreatedQueue(){
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
 }
---
+++ src/main/java/com/tecnologiaefinancas/orderms/dto/OrderCreatedEvent.java
@@ -0,0 +1,8 @@
+package com.tecnologiaefinancas.orderms.dto;
+
+import java.util.List;
+
+public record OrderCreatedEvent(Long codigoPedido,
+                                Long codigoCliente,
+                                List<OrderItemEvent> itens ) {
+}
